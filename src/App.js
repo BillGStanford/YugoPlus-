9aaaacf41872a5e9f11770aa9a4d4181851baafc
BillGStanford <164815569+BillGStanford@users.noreply.github.com>
2024-11-20 18:34:59 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ContactSection from './components/ContactSection';
 import AboutUsSection from './components/AboutUsSection';
 import Footer from './components/Footer';
 
-
 function App() {
   const [isVisible, setIsVisible] = useState(false);
   const [loading, setLoading] = useState(true); // Add loading state
@@ -39,46 +38,30 @@ function App() {
     return () => clearTimeout(timer); // Clean up the timer on component unmount
   }, []);
 
-
-
   return (
     <div className="bg-dark">
       <Navbar />
       
-      <div className="relative min-h-screen flex">
-        {/* Sidebar */}
-        <div className="fixed top-0 left-0 w-16 h-full bg-gradient-to-b from-primary to-secondary z-10 shadow-lg">
-          <div className="flex flex-col items-center justify-center space-y-6 h-full">
-            <a href="#hero" className="text-white text-2xl">🏠</a>
-            <a href="#services" className="text-white text-2xl">💼</a>
-            <a href="#work" className="text-white text-2xl">🖥️</a>
-            <a href="#about" className="text-white text-2xl">👥</a>
-            <a href="#contact" className="text-white text-2xl">📞</a>
-            <a href="#footer" className="text-white text-2xl">⚡</a>
-          </div>
-        </div>
-
-        <main className="w-full ml-16">
-          <section id="hero" className="min-h-screen">
-            <HeroSection />
-          </section>
-          <section id="services" className="min-h-screen">
-            <ServicesSection />
-          </section>
-          <section id="work" className="min-h-screen">
-            <WorkShowcase />
-          </section>
-          <section id="about" className="min-h-screen">
-            <AboutUsSection />
-          </section>
-          <section id="contact" className="min-h-screen">
-            <ContactSection />
-          </section>
-          <section id="footer">
-            <Footer />
-          </section>
-        </main>
-      </div>
+      <main>
+        <section id="hero" className="min-h-screen">
+          <HeroSection />
+        </section>
+        <section id="services" className="min-h-screen">
+          <ServicesSection />
+        </section>
+        <section id="work" className="min-h-screen">
+          <WorkShowcase />
+        </section>
+        <section id="about" className="min-h-screen">
+          <AboutUsSection />
+        </section>
+        <section id="contact" className="min-h-screen">
+          <ContactSection />
+        </section>
+        <section id="footer">
+          <Footer />
+        </section>
+      </main>
     </div>
   );
 }
